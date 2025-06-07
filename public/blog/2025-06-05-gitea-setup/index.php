b3b0b4a60a8a4204d2e5abd432d854313af2323e
nicoll-douglas <dev.nicoll.douglas@gmail.com>
2025-06-07 13:44:33 +0100

    udpate content

--- public/blog/2025-06-05-gitea-setup/index.php
+++ public/blog/2025-06-05-gitea-setup/index.php
@@ -2,39 +2,47 @@
 require __DIR__ . "/../../../src/partials/head.php";
 ?>
 
-<p>It's been a couple days but today is the day I set up Gitea. I've been doing a bit of reading of the Gitea docs and have added a <a href="https://github.com/nicoll-douglas/homelab/commit/161a8acce49c52ed87243c45c6c127bb69e29ff0">Docker Compose file</a> to the repo in accordance with the installation guide.</p>
-<p>From what I understand, if you want to interact with Gitea repos with SSH like you would regular GitHub repos, you have to make sure that you have a port exposed on your machine for that. You also need a HTTP port exposed in order to do the same with HTTP and to use the Gitea web interface. I've just stuck with 3000 for HTTP and 222 for SSH as defined in the Docker Compose file provided in their docs. These map to the necessary internal ports of the Docker container.</p>
+<section>
+  <h2>Setup</h2>
+  <p>It's been a couple days but today is the day I set up Gitea. I've been doing a bit of reading of the Gitea docs and have added a <a href="https://github.com/nicoll-douglas/homelab/commit/161a8acce49c52ed87243c45c6c127bb69e29ff0">Docker Compose file</a> to the repo in accordance with the installation guide.</p>
+  <p>From what I understand, if you want to interact with Gitea repos with SSH like you would regular GitHub repos, you have to make sure that you have a port exposed on your machine for that. You also need a HTTP port exposed in order to do the same with HTTP and to use the Gitea web interface. I've just stuck with 3000 for HTTP and 222 for SSH as defined in the Docker Compose file provided in their docs. These map to the necessary internal ports of the Docker container.</p>
 
-<p>Therefore, I've exposed these ports in <code>ufw</code> with the following commands:</p>
+  <p>Therefore, I've exposed these ports in <code>ufw</code> with the following commands:</p>
 
-<code><?php
-      $code = <<<BASH
-sudo ufw allow 3000/tcp
-sudo ufw allow from 192.168.1.6 to any port 222 proto tcp
-BASH;
-      echo $code
-      ?></code>
+  <code><?php
+        $code = <<<BASH
+  sudo ufw allow 3000/tcp
+  sudo ufw allow from 192.168.1.6 to any port 222 proto tcp
+  BASH;
+        echo $code
+        ?></code>
 
-<p>This now allows HTTP access from my network as well as SSH access from my main machine to Gitea on my laptop.</p>
-<p>Now, the next step is to start the container which should allow me to access the web interface and complete the installation. I've done that with <code>docker compose up -d</code> naturally.</p>
-<p>I tried navigating to 192.168.1.5:3000 in my browser and the web interface showed up which was huge. I proceeded with the final installation steps, created an account, and now Gitea was successfully set up.</p>
-<p>The next step was to generate an SSH key pair for me to use with Gitea from my main machine. I did that and then added the public key to my Gitea account in the web interface.</p>
-<p>Now it was time to test to see if SSH was working for the git user. I ran <code>ssh -p 222 git@192.168.1.5</code> and got a successful response which was good:</p>
+  <p>This now allows HTTP access from my network as well as SSH access from my main machine to Gitea on my laptop.</p>
+  <p>Now, the next step is to start the container which should allow me to access the web interface and complete the installation. I've done that with <code>docker compose up -d</code> naturally.</p>
+  <p>I tried navigating to 192.168.1.5:3000 in my browser and the web interface showed up which was huge. I proceeded with the final installation steps, created an account, and now Gitea was successfully set up.</p>
+  <p>The next step was to generate an SSH key pair for me to use with Gitea from my main machine. I did that and then added the public key to my Gitea account in the web interface.</p>
+  <p>Now it was time to test to see if SSH was working for the git user. I ran <code>ssh -p 222 git@192.168.1.5</code> and got a successful response which was good:</p>
 
-<img src="./successful-ssh.png" alt="Successful SSH" height="130">
+  <img src="./successful-ssh.png" alt="Successful SSH" height="130">
+</section>
 
-<p>The final thing I wanted to do is add the repo for this website as well as the homelab to Gitea. I created empty repositories in the Gitea web interface and then added a remote for each repo to point to it's Gitea repo. That was achieved with the following commands in each repo:</p>
-<code><?php
-      $code = <<<BASH
-git remote add gitea ssh://192.168.1.5:222/nicoll-douglas/homelab.git
-git remote add gitea ssh://192.168.1.5:222/nicoll-douglas/homelab-blog.git
-BASH;
-      echo $code
-      ?></code>
-<p>I then tried pushing my commits and it was successful for both repos:</p>
-<img src="./successful-push.png" height="200" alt="Successful push">
-<p>Now I have a successful self-hosted Gitea instance which is perfect for what comes next.</p>
+<section>
+  <h2>Repo Hosting & Push Mirroring</h2>
 
+  <p>The final thing I wanted to do is add the repo for this website as well as the homelab to Gitea and make sure they sync up with GitHub (in case my laptop dies or something). I created empty repositories for each in the Gitea web interface and then set the remote for each repo to point to their Gitea repo. That was achieved with the following commands:</p>
+  <code><?php
+        $code = <<<BASH
+  git remote set-url origin ssh://192.168.1.5:222/nicoll-douglas/homelab.git
+  git remote set-url origin ssh://192.168.1.5:222/nicoll-douglas/homelab-blog.git
+  BASH;
+        echo $code
+        ?></code>
+  <p>I then tried pushing my commits to test if I could indeed push to the remote in Gitea and it worked for both repos:</p>
+  <img src="./successful-push.png" height="180" alt="Successful push">
+
+  <p>The next step was to add a push mirror from Gitea to GitHub. This is a feature of Gitea that allows you to sync up your Gitea repo with a GitHub repo and so that whenever you push to Gitea, it will reflect that in GitHub. This was quite simple to do and just required me to enter the details of the mirror repo, my account, and a GitHub personal access token with read-write access into the Gitea repo settings. I then tried clicking the sync button and my newer commits showed up on GitHub—the push mirroring was now working.</p>
+  <p>Now I have Gitea all set up with some locally hosted repos which enables the next few things I want to set up for the homelab.</p>
+</section>
 
 <?php
 require __DIR__ . "/../../../src/partials/tail.php";
